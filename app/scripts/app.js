ccf51615372b3fe39f3110faef9d39640eea7cc6
supabok <superbok@superbokbok.com>
2013-01-13 12:28:21 +0900

    init commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var angularExpressYeomanApp = angular.module('angularExpressYeomanApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
